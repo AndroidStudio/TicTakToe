7bf92f78dec304acefcb4b3cc5c9ff2b0fdb06a3
Przemek <przemyslaw.skital@witchcraftstudios.com>
2018-07-20 16:06:12 +0200

    update

--- app/src/main/java/tic/tack/toe/arduino/game/CMD.java
+++ app/src/main/java/tic/tack/toe/arduino/game/CMD.java
@@ -3,11 +3,12 @@ package tic.tack.toe.arduino.game;
 public class CMD {
 
     /*
-    * Wartości w HEX
-    * */
-    public static final String BRIGHTNESS = "42";//ustawia jasność led
+     * Wartości w HEX
+     * */
+
+    public static final String BRIGHTNESS = "42";//ustawia jasność diody
     public static final String VIBRATE = "56";//wibracja
-    public static final String PIXEL = "50";//zapala led
-    public static final String RESET = "52";//gasi wszystkie ledy
+    public static final String PIXEL = "50";//zapala diodę
+    public static final String RESET = "52";//resetuje diody
 
 }
