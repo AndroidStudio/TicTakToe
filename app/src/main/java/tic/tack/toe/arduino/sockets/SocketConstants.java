27f7a3a5cd4aece0bf5af0c824802a3cf5a2a5b1
Przemek <przemyslaw.skital@witchcraftstudios.com>
2018-07-20 15:36:03 +0200

    update

--- app/src/main/java/tic/tack/toe/arduino/sockets/SocketConstants.java
+++ app/src/main/java/tic/tack/toe/arduino/sockets/SocketConstants.java
@@ -3,9 +3,23 @@ package tic.tack.toe.arduino.sockets;
 public class SocketConstants {
 
     public static final String TYPE = "type";
-    public static final String UDID = "udid";
-    public static final String INIT_GAME = "init_game";
+
     public static final String BLUETOOTH_ADDRESS = "bluetooth_address";
+    public static final String CURRENT_PLAYER = "current_player";
+    public static final String BRIGHTNESS = "brightness";
+    public static final String GAME_BOARD = "game_board";
+    public static final String INIT_GAME = "init_game";
     public static final String EXIT_GAME = "exit_game";
+    public static final String EXIT_UDID = "exit_udid";
+    public static final String NEW_GAME = "new_game";
+    public static final String WIN_UDID = "win_udid";
+    public static final String SYMBOL = "symbol";
+    public static final String STATUS = "status";
+    public static final String INDEX = "index";
+    public static final String COLOR = "color";
+    public static final String INFO = "info";
+    public static final String UDID = "udid";
+    public static final String WIN = "win";
+
 
 }
