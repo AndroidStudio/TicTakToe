92ca08faabd410691258da43849cb2857f497e4a
Przemek <przemyslaw.skital@witchcraftstudios.com>
2018-11-06 16:00:17 +0100

    update

--- app/src/main/java/tic/tack/toe/arduino/dialog/InputMACDialog.java
+++ app/src/main/java/tic/tack/toe/arduino/dialog/InputMACDialog.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import tic.tack.toe.arduino.MainActivity;
 import tic.tack.toe.arduino.R;
 import tic.tack.toe.arduino.ScanActivity;
 import tic.tack.toe.arduino.game.GameSettings;
@@ -26,19 +28,35 @@ public class InputMACDialog extends Dialog {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_mac_dialog);
 
-        final EditText macEditText = findViewById(R.id.macEditText);
+        final RadioGroup radioGroup = findViewById(R.id.radio_group);
         findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mac = macEditText.getText().toString();
-                if (!TextUtils.isEmpty(mac)) {
-                    GameSettings.getInstance().setMacAddress(mac);
-                    dismiss();
+                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                String mac = null;
+                switch (checkedRadioButtonId) {
+                    case R.id.radio_01:
+                        RadioButton radioButton_1 = findViewById(checkedRadioButtonId);
+                        mac = radioButton_1.getText().toString();
+                        break;
+                    case R.id.radio_02:
+                        RadioButton radioButton_2 = findViewById(checkedRadioButtonId);
+                        mac = radioButton_2.getText().toString();
+                        break;
+                }
 
-                    Intent intent = new Intent(context, ScanActivity.class);
+                dismiss();
+                if (TextUtils.isEmpty(mac)) {
+                    Intent intent = new Intent(context, MainActivity.class);
                     context.startActivity(intent);
                     context.finish();
+                    return;
                 }
+
+                GameSettings.getInstance().setMacAddress(mac);
+                Intent intent = new Intent(context, ScanActivity.class);
+                context.startActivity(intent);
+                context.finish();
             }
         });
     }
@@ -46,7 +64,7 @@ public class InputMACDialog extends Dialog {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        context.finish();
+        this.context.finish();
     }
 }
 
